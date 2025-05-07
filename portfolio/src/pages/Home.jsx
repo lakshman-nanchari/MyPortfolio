1f240c355d7236bd9e8a825c13d074dec6fdb73a
lakshman nanchari <lakshmannanchari96@gmail.com>
2025-05-07 16:43:53 +0530

    initial setup

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+export const Home = () => {
+    return (<div>
+        {/*Theme Toggle*/}
+
+        {/* Background Effects */}  
+
+        {/* Navbar */}
+
+        {/*main content*/}
+
+        {/* Footer */}
+    </div>)
+}
